740097b63c9e731f8b79cbb98f81643a106cf593
Connie Lee <connie.lee0317@gmail.com>
2016-08-02 14:56:01 -0500

    updates

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require( 'express' );
+var swig = require('swig');
+var routes = require('./routes/');
 var app = express(); // creates an instance of an express application
 
+//SWIG STUFF
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
+
+
+// MIDDLEWARE
+//logger
 app.use(function(req, res, next) {
 	console.log(req.method, req.url);
 	next();
@@ -13,8 +24,26 @@ app.use(function(req, res, next) {
 	next();
 })
 
-app.get('/', function(req, res) {
-	res.send("Welcome to twitter (rip-off)")
-})
+app.use(express.static('public'));
+app.use('/', routes);
+
+//ROUTING
+
+
+//---------old routes--------
+// var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// app.get('/views', function(req, res) {
+// 	res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
+
+
+// app.get('/', function(req, res) {
+// 	res.send("Welcome to twitter (rip-off)")
+// })
+// --------------------------
+
+
+
+
 
 app.listen(3000);
